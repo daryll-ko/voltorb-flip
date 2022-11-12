a170d82390fdb6522bdadb7d9381ff5ba7017f8a
Daryll Ko <daryll.ko@gmail.com>
2022-11-12 15:05:31 +0800

    feat: add template for flipState

--- src/GridSquare.js
+++ src/GridSquare.js
@@ -1,7 +1,20 @@
 import "./GridSquare.css";
 
+function flipGridSquareState() {
+	
+}
+
 function GridSquare({ gridSquareState }) {
-	return <div className="gridSquare">{gridSquareState}</div>;
+	return (
+		<div
+			className="gridSquare"
+			onClick={() => {
+				flipGridSquareState();
+			}}
+		>
+			{gridSquareState}
+		</div>
+	);
 }
 
 export default GridSquare;
