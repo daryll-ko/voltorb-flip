9cba507de90af91a30a780d1d93802dfe8ec3680
Daryll Ko <daryll.ko@gmail.com>
2022-11-13 20:56:45 +0800

    make opened cards bolder

--- src/GridSquare.js
+++ src/GridSquare.js
@@ -2,7 +2,10 @@ import "./GridSquare.css";
 
 function GridSquare({ index, faceUp, value, flipGridSquare }) {
 	return (
-		<div className="gridSquare" onClick={() => flipGridSquare(index)}>
+		<div
+			className={`gridSquare ${faceUp && "faceUp"}`}
+			onClick={() => flipGridSquare(index)}
+		>
 			<p>{faceUp ? value : "?"}</p>
 		</div>
 	);
