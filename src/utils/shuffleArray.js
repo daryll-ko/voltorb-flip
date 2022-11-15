aa3a8ea21c7341263cb8ad998b84a524545e4717
Daryll Ko <daryll.ko@gmail.com>
2022-11-15 17:20:03 +0800

    fix: correct index bound for `shuffleArray`

--- src/utils/shuffleArray.js
+++ src/utils/shuffleArray.js
@@ -1,7 +1,7 @@
 import generateInteger from "./rng";
 
 function shuffleArray(a, n) {
-	for (let i = 0; i < n; ++i) {
+	for (let i = 0; i < n - 1; ++i) {
 		const j = generateInteger(i + 1, n);
 		const copy = a[i];
 		a[i] = a[j];
