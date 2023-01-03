950481db0ca51dc38e6d39ee76fa742cc9239cc1
Daryll Ko <daryll.ko@gmail.com>
2023-01-03 15:17:19 +0800

    refactor: `Header.js` —> `Header.tsx`

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import logo from "../images/voltorb.png";
+import "./Header.css";
+
+function Header() {
+	return (
+		<header>
+			<img src={logo} alt="Voltorb logo" className="logo" />
+			<h1>Voltorb Flip</h1>
+		</header>
+	);
+}
+
+export default Header;
