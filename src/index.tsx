1652cab90a5d33641c408ec4c052487e8335ed3b
Daryll Ko <daryll.ko@gmail.com>
2023-01-03 15:54:01 +0800

    refactor: make `index` root initialization cleaner

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
-if (!rootElement) {
-	throw new Error("Failed to find the root element.");
-}
-const root = createRoot(rootElement);
+const root = createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
