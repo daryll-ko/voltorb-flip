131099d2ce7d7406e849b49f202ccda016d81811
Daryll Ko <daryll.ko@gmail.com>
2023-01-03 15:14:43 +0800

    feat: fully type `App`

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header.tsx";
+import Main from "./Main.tsx";
+import Footer from "./Footer.tsx";
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<Main />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
