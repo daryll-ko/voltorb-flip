1b478a732c2538c463c55fff773acebd89ef5e2e
Daryll Ko <daryll.ko@gmail.com>
2023-01-03 15:23:11 +0800

    feat: fully type `GridSquare`

---
+++ src/components/GridSquare.tsx
@@ -0,0 +1,35 @@
+import "./GridSquare.css";
+import voltorb from "../images/voltorb.png";
+import { GridSquareState } from "../utils/types";
+
+function GridSquare({
+	index,
+	faceUp,
+	value,
+	gameOver,
+	gameClear,
+	flipGridSquare,
+}: GridSquareState) {
+	return (
+		<div
+			className={`gridSquare ${faceUp ? "faceUp" : ""} ${
+				!(faceUp || gameOver || gameClear) ? "clickable" : ""
+			} ${!faceUp && (gameOver || gameClear) ? "aftermath" : ""} ${
+				faceUp && value === 0 ? "offender" : ""
+			}`}
+			onClick={() => flipGridSquare(index)}
+		>
+			{value === 0 ? (
+				faceUp || gameOver || gameClear ? (
+					<img src={voltorb} alt="Voltorb" className="voltorb" />
+				) : (
+					<p>?</p>
+				)
+			) : (
+				<p>{faceUp || gameOver || gameClear ? value : "?"}</p>
+			)}
+		</div>
+	);
+}
+
+export default GridSquare;
