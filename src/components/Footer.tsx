d946cfe33bfd3d88b68501cc9b5832a1c547a018
Daryll Ko <daryll.ko@gmail.com>
2023-04-11 10:21:30 +0800

    fix: use bigger copyright symbol

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="w-full bg-[#188060] p-4 text-center font-bold text-white">
-      © 2023 Daryll Ko
+      ⓒ 2023 Daryll Ko
     </footer>
   );
 }
