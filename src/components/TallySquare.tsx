5883f444fe24cec11529fb32f2dbd06725b9a75b
Daryll Ko <daryll.ko@gmail.com>
2023-04-11 10:34:00 +0800

    feat: use new coin + voltorb image

--- src/components/TallySquare.tsx
+++ src/components/TallySquare.tsx
@@ -1,4 +1,4 @@
-import coins from "../images/coins.webp";
+import coin from "../images/coin.png";
 import voltorb from "../images/voltorb.png";
 
 interface Props {
@@ -12,11 +12,11 @@ function TallySquare({ sum, voltorbCount, color }: Props) {
     <div
       className={`z-10 flex h-[60px] w-[60px] flex-col rounded-md border-4 border-solid border-white md:h-[80px] md:w-[80px] ${color}`}
     >
-      <div className="flex h-[50%] items-center border-b-2 border-solid border-white p-1">
-        <img src={coins} alt="Coin bag" className="h-full" />
+      <div className="flex h-[50%] items-center justify-between border-b-2 border-solid border-white px-2 py-1">
+        <img src={coin} alt="Coin" className="h-full" />
         <p>{sum}</p>
       </div>
-      <div className="flex h-[50%] items-center border-t-2 border-solid border-white p-1">
+      <div className="flex h-[50%] items-center justify-between border-t-2 border-solid border-white px-2 py-1">
         <img src={voltorb} alt="Voltorb" className="h-full" />
         <p>{voltorbCount}</p>
       </div>
