4c51f74194f07fb8bce70d2118486923c34e28d5
Daryll Ko <daryll.ko@gmail.com>
2023-04-11 10:38:55 +0800

    fix: adjust header spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import logo from "../images/voltorb.png";
 
 function Header() {
   return (
-    <header className="flex items-center justify-center gap-2 bg-[#188060] p-2 text-white">
-      <img src={logo} alt="Voltorb logo" className="h-[25px] w-[25px]" />
+    <header className="flex items-center justify-center gap-4 bg-[#188060] p-4 font-bold text-white">
+      <img src={logo} alt="Voltorb logo" className="h-[30px] w-[30px]" />
       <h1>Voltorb Flip</h1>
     </header>
   );
