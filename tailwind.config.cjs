55f7c5f7c3cc60e67e2a4b50a46ca92eeca2f59a
Daryll Ko <daryll.ko@gmail.com>
2023-07-28 23:28:11 +0800

    refactor: use Nunito as font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      display: ["Space Mono"],
+      display: ["Nunito"],
     },
     extend: {},
   },
