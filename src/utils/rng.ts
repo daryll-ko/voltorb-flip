24efec9a084b05916537e7b94cb5cb80c90e139f
Daryll Ko <daryll.ko@gmail.com>
2023-10-31 13:08:54 +0800

    update: make `export function` for `generateInteger` inline

--- src/utils/rng.ts
+++ src/utils/rng.ts
@@ -1,6 +1,4 @@
-function generateInteger(low: number, high: number): number {
+export function generateInteger(low: number, high: number): number {
   // [low, high)
   return low + Math.floor(Math.random() * (high - low));
 }
-
-export default generateInteger;
