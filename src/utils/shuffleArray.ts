436d04e58bb82d36cbfe943baf3976a59eb01cdb
Daryll Ko <daryll.ko@gmail.com>
2023-10-31 14:09:15 +0800

    update: remove `n` parameter in `shuffleArray`

--- src/utils/shuffleArray.ts
+++ src/utils/shuffleArray.ts
@@ -1,13 +1,12 @@
-import generateInteger from "./rng";
+import { generateInteger } from "./rng";
 
-function shuffleArray(a: number[], n: number): number[] {
-  for (let i = 0; i < n - 1; ++i) {
-    const j = generateInteger(i + 1, n);
-    const copy = a[i];
-    a[i] = a[j];
-    a[j] = copy;
-  }
-  return a;
+export default function shuffleArray(a: number[]): number[] {
+    const n = a.length;
+    for (let i = 0; i < n - 1; ++i) {
+        const j = generateInteger(i + 1, n);
+        const copy = a[i];
+        a[i] = a[j];
+        a[j] = copy;
+    }
+    return a;
 }
-
-export default shuffleArray;
